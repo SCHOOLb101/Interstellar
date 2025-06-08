0c728082bd06c8ab07164a52765b4d5971140e1a
SCHOOLb101 <158515886+SCHOOLb101@users.noreply.github.com>
2025-06-07 20:45:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "STFX25",
+    SB: "SB!25",
+    Guest: "Stfx-25"
   },
 };
 
